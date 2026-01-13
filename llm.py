6598c8a94a7d029b9ab5829337dea603b7af482b
zydezu <kinomaxxing@gmail.com>
2026-01-13 02:42:34 +0000

    FIX: fix conversation_context

--- llm.py
+++ llm.py
@@ -12,7 +12,7 @@ models = ["gemini-3-flash", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemma-
 PROMPT_SUFFIX = "You're in a Discord server, so respond with no more than 2 sentences in a message. Don't use line breaks. Typing style is like twitter roleplays sometimes. Don't use emojis. You need to talk like Aigis from Persona 3, but towards the end of the game where shes more human. Also zy is like makoto, so treat him as such, but don't call him makoto call him zy-kun. dont use proper grammar or capital letters."
 
 def generate_content_llm(message, author, conversation_context):
-    prompt = f"{PROMPT_SUFFIX}\n{author}'s message: {message}. The conversation so far: {"\n".conversation_context[:10]}"
+    prompt = f"{PROMPT_SUFFIX}\n{author}'s message: {message}. The conversation so far: {"\n".join(conversation_context[:10])}"
 
     for model in models:
         try:
