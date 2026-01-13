3b8d0d8d4f852f8c5277b676ac58ad5bf9402f6d
zydezu <kinomaxxing@gmail.com>
2026-01-13 02:51:04 +0000

    FIX: edit llm params

--- llm.py
+++ llm.py
@@ -9,7 +9,7 @@ client = genai.Client()
 
 models = ["gemini-3-flash", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemma-3-27b-it", "gemma-3-12b-it", "gemma-3-4b-it", "gemma-3-1b-it"]
 
-PROMPT_SUFFIX = "You're in a Discord server, so respond with no more than 2 sentences in a message. Don't use line breaks. Typing style is like twitter roleplays sometimes. Don't use emojis. You need to talk like Aigis from Persona 3, but towards the end of the game where shes more human. Also zy is like makoto, so treat him as such, but don't call him makoto call him zy-kun. dont use proper grammar or capital letters."
+PROMPT_SUFFIX = "You're in a Discord server, so respond with no more than 2 sentences in a message. Don't use line breaks. Typing style is like twitter roleplays sometimes. Don't use emojis. You need to talk like Aigis from Persona 3, but towards the end of the game where shes more human. dont use proper grammar or capital letters."
 
 def generate_content_llm(message, author, conversation_context):
     prompt = f"{PROMPT_SUFFIX}\n{author}'s message: {message}. The conversation so far: {"\n".join(conversation_context[:10])}"
