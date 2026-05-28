beaa2ec0100fad024b00ae482ef532fcf0bd8094
zydezu <kinomaxxing@gmail.com>
2026-05-28 21:36:14 +0100

    FIX: update .gitignore

--- cogs/fun_cog.py
+++ cogs/fun_cog.py
@@ -2,32 +2,14 @@ import os
 
 import aiohttp
 import discord
-from discord import app_commands
 from discord.ext import commands
 
-import embed as embed_module
 import scripts.danboorusearch as danboorusearch
-import scripts.graphics as graphics
 
 
 class FunCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @app_commands.command(
-        name="accentcolour", description="Get the accent colour of a user's avatar"
-    )
-    @app_commands.describe(user="The user to get the accent colour from (optional)")
-    async def accentcolour(
-        self, interaction: discord.Interaction, user: discord.User | None = None
-    ):
-        target_user = user or interaction.user
-        hex_color, color_image = graphics.get_accent_colour(target_user)
-        embed_to_send, file = embed_module.show_accent_colour(hex_color, color_image)
-        try:
-            await interaction.response.send_message(embed=embed_to_send, file=file)
-        except aiohttp.ClientConnectionResetError:
-            pass
 
     @commands.command()
     async def hi(self, ctx):
